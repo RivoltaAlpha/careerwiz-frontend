818887e46118f9ad4ed1006959ce992bd541a7e2
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-12-29 15:31:46 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+
+      colors: {
+        'secondary': '#003366',
+        'cards':'#116696',
+        'primary':'#B4D4FF',
+        'gray-100': '#f5f5f5',
+        'base': '#0D6D82', 
+        'bg': '#000d1a',
+         },
+    },
+  },
+  plugins: [],
+}
