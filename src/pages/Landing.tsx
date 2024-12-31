00396e9ceba7e437d643ac2709c2d31987890c2c
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-12-31 12:29:36 +0300

    Landing page

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,19 @@
+import Hero from "../components/Hero";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Main from "../components/Main";
+
+const Landing = () => {
+    return (
+    <div className="flex flex-col bg-gray-900 min-h-screen">
+        <main className="flex-grow">
+            <Header />
+            <Hero />
+            <Main />
+            <Footer />
+        </main> 
+    </div>
+    )
+}
+
+export default Landing
