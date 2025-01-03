d7ce0958c6194ec23d1d1d61c55e3c9c7dde6785
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-01-03 20:44:17 +0300

    types

---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+export interface CareerCardProps {
+    id: number;
+    image: string   ;
+    CareerName: string;
+    description: string;
+    requirements: string[];
+    subjects: string[];
+    interests: string[];
+}
