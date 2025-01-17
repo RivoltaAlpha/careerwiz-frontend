316efe0285f886ec92a9d1358b9b44048fa20800
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-01-17 14:19:29 +0300

    Add contact section with email form and notifications

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Landing from './pages/Landing'
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Feedback from './pages/Feedback';
+import ContactSection from './pages/ContactUs';
 
 import './index.css'
 
@@ -18,6 +19,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/explore" element={<Register />} />
           <Route path="/feedback" element={<Feedback />} />
+          <Route path="/contact" element={<ContactSection />} />
         </Routes>
       </div>
       </Router>
