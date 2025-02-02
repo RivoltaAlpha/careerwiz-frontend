f7f0630ba2083bf6a6b90723359819fa4b291743
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-02-02 11:46:35 +0300

    types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,5 @@
 export interface CareerCardProps {
-    id: number;
+    career_id: number;
     image: string   ;
     CareerName: string;
     description: string;
@@ -7,8 +7,57 @@ export interface CareerCardProps {
     subjects: string[];
     interests: string[];
 }
+export interface Career {
+    career_id: number;
+    CareerName: string;
+    description: string;
+    requirements: string[];
+    subjects: string[];
+    interests: string[];
+}
+  // Login Response
+  export interface LoginResponse {
+    username: string;
+    password: string;    
+}
+
+  // Login User
+  export interface Luser {
+    username: string;
+    password: string;
+    token: string; 
+   }
 
 export interface Careers {
     career: CareerCardProps[];
 }
 
+  // create User
+  export interface TUser {
+    user_id: number;
+    username: string;
+    email: string;
+    password: string;
+    role: string;
+    image_url: string;
+}
+
+export interface UserAuthenticatedState {
+    user:{
+        user_id: number
+        username: string
+        email: string
+        role: string
+        image_url: string
+    } | null
+    token: string | null
+    isAuthenticated: boolean
+    loading: boolean
+    error: string | null
+  }
+
+  export interface UserState {
+    user: TUser | null;
+    loading: boolean;
+    error: string | null;
+  }
