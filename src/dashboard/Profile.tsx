dde84d2a152632bdff78c2a6ba9553799fde8fc5
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-03-18 01:07:01 +0300

    improve text visibility

--- src/dashboard/Profile.tsx
+++ src/dashboard/Profile.tsx
@@ -6,14 +6,14 @@ import { Link } from 'react-router-dom';
 export default function Profile() {
     const { user } = useSelector((state: RootState) => state.userAuth);
     return (
-        <div className="flex min-h-screen bg-gray-100">
+        <div className="flex min-h-screen bg-gray-100 text-white">
             <Sidebar />
             <div className="max-w-2xl mx-auto p-8">
                 <div className="bg-white shadow-md rounded-lg w-[500px] overflow-hidden">
                     <div className="p-6">
                         <div className="flex items-center justify-center mb-6">
                             <img
-                                src={user?.image_url || '/images/Social media-cuate.png'}
+                                src={'/images/Social media-cuate.png'}
                                 alt="User Profile"
                                 className="w-32 h-32 rounded-full object-cover"
                             />
