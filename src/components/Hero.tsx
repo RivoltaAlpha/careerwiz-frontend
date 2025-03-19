f70eab16585299ecf6162fbbc46050116682f273
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-03-19 22:03:06 +0300

    recommendation routes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero: React.FC = () => {
           An easier way to identify what career suits you most based on your Academics and Personal Interests.
         </p>
         <a 
-          href="/login" 
+          href="/predictions" 
           className="bg-cards hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300"
         >
           Get Started
