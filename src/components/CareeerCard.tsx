13298d4fbb8a5f60b5255c82be8e3418cdebd048
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-03-26 07:23:34 +0300

    Adding Items to cart

--- src/components/CareeerCard.tsx
+++ src/components/CareeerCard.tsx
@@ -3,6 +3,8 @@ import { CareerCardProps } from "../types/types";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
+import { BiCart, BiNotepad } from "react-icons/bi";
+import { ToastContainer, toast } from "react-toastify";
 
 const CareerCard: React.FC<CareerCardProps> = ({ career_id, career_name, image, interests }) => {
   const { isAuthenticated } = useSelector((state: RootState) => state.userAuth);
@@ -18,8 +20,16 @@ const CareerCard: React.FC<CareerCardProps> = ({ career_id, career_name, image, 
     navigate(`/career-info/${career_id}`);
   };
 
+  const addToCart = () => {}
+    if(isAuthenticated){
+      
+    }else {
+      toast("Login to add items to cart")
+    }
+
   return (
     <div className="bg-white shadow-lg rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
+      <ToastContainer/>
       <div className="md:flex">
         <div className="md:flex-shrink-0">
           <img className="w-[500px] object-cover md-w-full" src={image} alt={career_name} />
@@ -35,12 +45,22 @@ const CareerCard: React.FC<CareerCardProps> = ({ career_id, career_name, image, 
               </span>
             ))}
           </h3>
-          <button
-            className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-800 transition duration-300"
-            onClick={handleExplore}
-          >
-            Explore
-          </button>
+          <div className="flex gap-2 items-center justify-center">
+            <button
+              type="button"
+              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-800 transition duration-300"
+              onClick={handleExplore}>
+              <BiNotepad/>
+              Explore
+            </button>
+            <button
+              type="button"
+              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-800 transition duration-300"
+              onClick={addToCart}>
+              <BiCart/>
+              Add 
+            </button>
+          </div>
         </div>
       </div>
     </div>
