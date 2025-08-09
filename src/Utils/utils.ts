df0309ff655f581b9e3d31d6fa429e3db2fcdd1c
Mwaniki Tifany <111235348+RivoltaAlpha@users.noreply.github.com>
2025-08-09 20:51:32 +0300

    Update model link

--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -1,2 +1,2 @@
 export const SERVER_URL = "https://careerwizard-ewgec8e8dme9ggep.southafricanorth-01.azurewebsites.net/";
-export const MODEL_URL = "https://recommendationmodel-fbarbzdsczhqhphb.southafricanorth-01.azurewebsites.net/";
+export const MODEL_URL = "https://careerwiz-ai-d0a0fshfgsfmgwf4.southafricanorth-01.azurewebsites.net/";
